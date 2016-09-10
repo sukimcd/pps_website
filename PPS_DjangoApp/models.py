dd778ba560efa673270edfd417e78b3b25622b6c
Suki McDonough <mcdonough.suki@gmail.com>
2016-09-09 21:13:47 -0700

    Committing addition of 'middleware' folder and changes to 'models.py' and 'chat.html' modules that accomodate authentication by middleware.

--- PPS_DjangoApp/models.py
+++ PPS_DjangoApp/models.py
@@ -2,10 +2,76 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-STATES=(('key','arg'),)
+STATES = (
+    ('AK', 'Alaska'),
+    ('AL', 'Alabama'),
+    ('AR', 'Arkansas'),
+    ('AS', 'American Samoa'),
+    ('AZ', 'Arizona'),
+    ('CA', 'California'),
+    ('CO', 'Colorado'),
+    ('CT', 'Connecticut'),
+    ('DC', 'District of Columbia'),
+    ('DE', 'Delaware'),
+    ('FL', 'Florida'),
+    ('FM', 'Federated States of Microsnesia'),
+    ('GA', 'Georgia'),
+    ('GU', 'Guam'),
+    ('HI', 'Hawaii'),
+    ('IA', 'Iowa'),
+    ('ID', 'Idaho'),
+    ('IL', 'Illinois'),
+    ('IN', 'Indiana'),
+    ('KS', 'Kansas'),
+    ('KY', 'Kentucky'),
+    ('LA', 'Louisiana'),
+    ('MA', 'Massachusetts'),
+    ('MD', 'Maryland'),
+    ('ME', 'Maine'),
+    ('MI', 'Michigan'),
+    ('MN', 'Minnesota'),
+    ('MO', 'Missouri'),
+    ('MP', 'Northern Mariana Islands'),
+    ('MS', 'Mississippi'),
+    ('MT', 'Montana'),
+    ('NA', 'National'),
+    ('NC', 'North Carolina'),
+    ('ND', 'North Dakota'),
+    ('NE', 'Nebraska'),
+    ('NH', 'New Hampshire'),
+    ('NJ', 'New Jersey'),
+    ('NM', 'New Mexico'),
+    ('NV', 'Nevada'),
+    ('NY', 'New York'),
+    ('OH', 'Ohio'),
+    ('OK', 'Oklahoma'),
+    ('OR', 'Oregon'),
+    ('PA', 'Pennsylvania'),
+    ('PR', 'Puerto Rico'),
+    ('RI', 'Rhode Island'),
+    ('SC', 'South Carolina'),
+    ('SD', 'South Dakota'),
+    ('TN', 'Tennessee'),
+    ('TX', 'Texas'),
+    ('UT', 'Utah'),
+    ('VA', 'Virginia'),
+    ('VI', 'Virgin Islands'),
+    ('VT', 'Vermont'),
+    ('WA', 'Washington'),
+    ('WI', 'Wisconsin'),
+    ('WV', 'West Virginia'),
+    ('WY', 'Wyoming')
+  )
+
 
 class Member(models.Model):
     user = models.OneToOneField(User)
     birthdate = models.DateField(auto_now_add=False)
+    gender = models.TextField(null=True, blank=True)
+    pronouns = models.TextField(null=True, blank=True)
     issubscribed = models.BooleanField(default=False)
-    resstate = models.CharField(max_length=30, choices=STATES)
+    resstate = models.CharField(max_length=2, choices=STATES)
+    bio = models.TextField()
+
+    def __str__(self):
+        return "{}, {}".format(self.user.username, self.resstate)
